571bf82f4de5f9e23a9a3d86e1f4599b7a5f52c1
Maksymyr <maksymyr@gmail.com>
2017-11-01 14:57:44 +0200

    together

---
+++ src/js/components/Book.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+const mapStateToProps = (state, ownProps) => {
+    return {books: state.books}
+}
+
+@connect (mapStateToProps)
+export default class Book extends React.Component {
+    render() {
+        console.log(this.props.item);
+            return (
+            <div className="book">
+                <div className="book-inner">
+                    <p>{this.props.item.name}</p>
+                    
+                </div>
+            </div>
+        )
+    }
+}
