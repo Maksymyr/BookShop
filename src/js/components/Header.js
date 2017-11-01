1eef5eb7c2ab379e7db2288ff58d0e48d0f9b78f
pereZZZ <perezzz@meta.ua>
2017-11-01 11:31:43 +0200

    headmaid

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,26 +1,28 @@
 import React from 'react';
-
 import Menu from '../components/Menu';
 import Slider from '../components/Slider';
+import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router';
 
+@withRouter
+export default class MainLayout extends React.Component {
 
-const Header = (props) => (
-    <header className="header">
-        <h1 className="page-title">React Lesson: 4 [ context, Children API ]</h1>
-     
-        <Slider>
-            <p>Степа</p>
-            <p>Препод</p>
-            <p>У нас</p>
-            <p>А Максим - </p>
-            <p>МО</p>
-            <p>ЛО</p>
-            <p>ДЕЦ!</p>
-            
-            
-        </Slider>
-        <Menu/>
-    </header>
-);
+    search=(event)=>{
+        if (event.key === "Enter") {
+            let t= this.refs.search.value
+            this.refs.search.value='';
+            return (this.props.history.push(`/search/${t}`))
+        }
+    }
 
-export default Header;
+    render(){
+        return(
+            <header className="header">
+                <Link to='/'><h1 className="page-title">Book Shop</h1></Link>
+                <input type='text' onKeyPress={this.search} className='search' ref='search'/>
+                <Link to='/basket'><div className='basket'></div></Link>
+                <Link to='/room'><div className='room'></div></Link>
+            </header>
+        );
+    }
+}
