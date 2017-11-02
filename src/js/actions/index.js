32c0ef4d10ba64f75768ccbeb3d96ca460cfac84
Maksymyr <maksymyr@gmail.com>
2017-11-02 15:53:47 +0200

    search

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,6 +1,4 @@
 import * as types from '../constants/ActionTypes';
 
-// // Actions here
-// export const addPost = (payload) => ({type: types.ADD_POST, payload});
-// export const updateContentToggler = (payload) => ({type: types.UPDATE_CONTENT_TOGGLER, payload}); 
 export const futureBook = (payload) => ({type: types.FEATURUDE_BOOK, payload}); 
+export const searchBook = (payload) => ({type: types.SEARCH, payload});
