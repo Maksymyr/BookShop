cffa7efb832b7e7f31c3e3df8986b36a9977ddd4
Vladimir <prhvladimir@gmail.com>
2017-11-02 12:19:13 +0200

    'heart'

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -3,3 +3,4 @@ import * as types from '../constants/ActionTypes';
 // // Actions here
 // export const addPost = (payload) => ({type: types.ADD_POST, payload});
 // export const updateContentToggler = (payload) => ({type: types.UPDATE_CONTENT_TOGGLER, payload}); 
+export const futureBook = (payload) => ({type: types.FEATURUDE_BOOK, payload}); 
--- src/js/components/Book.js
+++ src/js/components/Book.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import { bindActionCreators} from 'redux';
+import {futureBook} from '../actions';
 
-
+const mapDispatchToProps = dispatch => ( bindActionCreators({ futureBook }, dispatch) );
 
 const mapStateToProps = (state, ownProps) => {
     return {books: state.books, futured: state.futured}
 }
 
-@connect (mapStateToProps)
+@connect (mapStateToProps, mapDispatchToProps)
 export default class Book extends React.Component {
 
     state = {
         flag: 0,
 
     }
-    handleEmpty = () => {
-        this.refs.futures.style.background = 'url('+ require("../../icon/heart-empty1.png")+')';
-        this.refs.futures.style.backgroundSize= "100% 100%"
-        //+require("../../icon/bascket.png")+
-    }
+    // handleEmpty = () => {
+    //     this.refs.futures.style.background = 'url('+ require("../../icon/heart-empty1.png")+')';
+    //     this.refs.futures.style.backgroundSize= "100% 100%"
+    //     //+require("../../icon/bascket.png")+
+    // }
 
-    handleFill = () => {
-       // console.log(this.refs.futures);
+    // handleFill = () => {
+    //    // console.log(this.refs.futures);
     
-        this.refs.futures.style.background = 'url('+ require("../../icon/heart-fill.png")+')';
-        this.refs.futures.style.backgroundSize= "100% 100%"
+    //     this.refs.futures.style.background = 'url('+ require("../../icon/heart-fill.png")+')';
+    //     this.refs.futures.style.backgroundSize= "100% 100%"
         
-    }
+    // }
 
     handleClick = () => {
         console.log(this.props.item.futured)
         console.log(this.props.index)
+        this.props.futureBook(this.props.index)
         
     }
 
@@ -41,7 +44,9 @@ export default class Book extends React.Component {
             <div className="book">
                 <p className="book_price">{this.props.item.price + " грн."}</p>
                 <div className="book-inner">
-                     <div className="future" ref="futures" style={{backgroundImage: 'url('+ require("../../icon/heart-empty1.png")+')'}} onClick={this.handleClick} onMouseEnter ={this.handleFill} onMouseLeave={this.handleEmpty}></div> 
+
+                     <div className="future" ref="futures" style={this.props.item.futured?{backgroundImage: 'url('+ require("../../icon/heart-fill.png")+')'}:{backgroundImage: 'url('+ require("../../icon/heart-empty1.png")+')'} } onClick={this.handleClick} onMouseEnter ={this.handleFill} onMouseLeave={this.handleEmpty}></div> 
+
                     <Link to="/"><img src={this.props.item.img}/></Link>
                     <p className="book_text"><Link className="book_link" to="/">{this.props.item.name}</Link></p>
                     <p className="book_author">{this.props.item.author}</p>
