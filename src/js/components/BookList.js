1776d5f5088f84ca1a8983ad9cc29d561574300f
pereZZZ <perezzz@meta.ua>
2017-11-06 13:21:11 +0200

    booklist

--- src/js/components/BookList.js
+++ src/js/components/BookList.js
@@ -45,20 +45,6 @@ const mapStateToProps = (state, ownProps) => {
 
 @connect (mapStateToProps, mapDispatchToProps)
 export default class BookList extends React.Component {
-    
-    page = () =>{
-        if(this.props.books.length>20){
-            let a=[];
-            for(var i=0; i<Math.floor(this.props.books.length/20); i++){
-                if(a[i]=='undefined'){
-                    a[i]=<Link to={`/page${i+2}`}><p key={i} >{i+2}</p></Link>
-                }else{
-                    a[i]=<Link to={`/page${i+2}`}><p key={i}>{i+2}</p></Link>
-                }
-            }
-            return a.map((item)=>{return item});
-        }
-    }    
     constructor(props){
         super(props);
         this.state = {
@@ -114,41 +100,12 @@ export default class BookList extends React.Component {
                 <div>
                     <Category />                
                     <div className="book-list-main">
-                        <div id={this.props.sidebar? "w77" : "w96"} className="book-list" ref="book_list">
-                        {!!this.props.match.params.id ? 
-                            this.props.books.map((item, index) => {
-                                if(this.props.match.url.replace(/\d/g, '') == '/page'){
-                                    return <Book item={item} key={index} index={index}/>
-                                }
-                                if (this.props.category[this.props.match.params.id] == item.type) {
-                                   return <Book item={item} key={index} index={index}/>
-                                } else if(this.props.match.params.id == "love"){
-                                    if(item.futured == true){
-                                        return <Book item={item} key={index} index={index}/>
-                                    }
-                                }
-                                else if(this.props.match.params.id == "l_d"){
-                                    if(item.futured == true){
-                                        return <Book item={item} key={index} index={index}/>
-                                    }
-                                }
-                                if (this.props.search != "") {
-                                    if (item.name.toLowerCase().includes(this.props.match.params.id.toLowerCase())
-                                    ||item.author.toLowerCase().includes(this.props.match.params.id.toLowerCase())
-                                    ||item.seria.toLowerCase().includes(this.props.match.params.id.toLowerCase())) {
-                                        return <Book item={item} key={index} index={index}/>
-                                    }
-                                }
-                            }) :
-                            this.props.books.map((item, index) =><Book item={item} key={index} index={index}/>)}
-                            {this.page()}
                         <div id={this.props.sidebar? "w77" : "w96"}  className="book-list"  ref="book_list">
                             {this.state.books.map((item, index) => <Book item={item} key={index} index={index}/>)}
                         </div>
                     </div>
                  </div>
             </div>
-        </div>    
         )
     }
 }
