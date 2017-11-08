02bd26978ef5b6c3d4d6723a5ecfc6053a0e9e87
Maksymyr <maksymyr@gmail.com>
2017-11-08 16:34:13 +0200

    red

--- src/js/components/BookPage.js
+++ src/js/components/BookPage.js
@@ -70,9 +70,6 @@ export default  class BookPage extends React.Component{
         return stars;
     }
     render(){
-
-        //console.log(this.props.match.params.id);
-        console.log(this.props.item)
         return(
             <div className="book_page">
                 
@@ -122,7 +119,7 @@ export default  class BookPage extends React.Component{
                     
                     { this.props.item.comments.length != 0 ? this.props.item.comments.map((item, index) => 
                     <div className="comment" key={index}> 
-                        <BookPageComment book={this.props.item.code} id={index} item={item}/>
+                        <BookPageComment book={this.props.item} id={index} item={item}/>
                     </div>) : null} 
 
 
