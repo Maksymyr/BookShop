d80df2fee4553bd1c0bd9d312c6679f5333b7e06
Maksymyr <maksymyr@gmail.com>
2017-11-18 15:15:29 +0200

    categ

--- src/js/components/BookList.js
+++ src/js/components/BookList.js
@@ -16,7 +16,7 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (state, ownProps) => {
    
-
+    console.log(ownProps.match.params)
     if(ownProps.match.params.id) {
         if(ownProps.match.url.replace(/[^a-z]/g, '')=='pages'){
             if(document.documentElement.clientWidth > 852){
